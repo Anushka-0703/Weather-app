47662aee17678cf3e9bcf9ed0cc4097f9043473b
Anushka-0703 <anushka.970747@gmail.com>
2026-06-15 05:04:03 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const inputBox = document.querySelector(".input-box");
+const searchBtn = document.getElementById("search-btn");
+const weatherIcon = document.querySelector(".weather-icon");
+const temp = document.querySelector(".temp");
+const city = document.querySelector(".city");
+const description = document.querySelector(".description");
+const humidity = document.querySelector(".humidity");
+const wind = document.querySelector(".wind");
+
+async function checkWeather(cityName) {
+     const apiKey = "589556147f21918c42bb14483eb11b2e"; 
+     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+     try {
+          const response = await fetch(url);
+          if (!response.ok) throw new Error("City not found");
+          const weatherData = await response.json();
+
+          
+          temp.innerHTML = `${Math.round(weatherData.main.temp)}°C`;
+          city.innerHTML = `${weatherData.name}, ${weatherData.sys.country}`;
+          description.innerHTML = weatherData.weather[0].description;
+          humidity.innerHTML = `${weatherData.main.humidity}%`;
+          wind.innerHTML = `${weatherData.wind.speed} km/h`;
+
+          const iconCode = weatherData.weather[0].icon;
+          weatherIcon.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png?`;
+     } catch (error) {
+          alert("City not found! Please try again.");
+     }
+}
+
+searchBtn.addEventListener("click", () => {
+     const cityName = inputBox.value.trim();
+     if (cityName !== "") {
+          checkWeather(cityName);
+     } else {
+          alert("Please enter a city name!");
+     }
+});
